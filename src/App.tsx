b41ce5c440a27f7c645b7fef89bb0fd250118413
Aldis <aldisgailis1@gmail.com>
2022-01-02 13:37:35 -0800

    Added IMG support

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import './app.css';
+import ReactLogo from './react.svg';
+
 const App = () => {
   console.log('All is good!');
   return (
-    <h1>
-     Template with TS(strict on)! 
-    </h1>
+    <div className="wrapper">
+      <h1>
+        Template with TS(strict on)! 
+      </h1>
+      <img src={ReactLogo} alt="reactImage" width="200px" />
+      <ul>
+        <li>Es Lint - nope</li>
+        <li>SCSS - nope</li>
+        <li>Styled Components - nope</li>
+        <li>Redux@Toolkit - nope</li>
+        <li>ESLint - nope</li>
+      </ul>
+    </div>
   )
 }
 
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: path.resolve(__dirname, '..', './src/index.tsx'),
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.tsx', '.ts', '.js',],
   },
   module: {
     rules: [
@@ -14,9 +14,21 @@ module.exports = {
         use: [
           {
             loader: 'babel-loader',
-          }
-        ]
-      }
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
     ]
   },
   output: {
